250425e7d25a1ef7a281b2cfacf54774a705529a
Javier Cortejoso <javier.cortejoso@gmail.com>
2024-10-09 19:00:24 +0200

    Remove extra space

--- hello.js
+++ hello.js
@@ -1,4 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola mundo!... saludos desde Peru...";
 console.log(hello());
 exports.hello = hello;
-
